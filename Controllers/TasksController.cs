f6b6f2e042072421a75d6fe4e88de351002c3303
murillo.nahas <murillonahas@hotmail.com>
2023-09-01 17:08:45 -0300

    task controller by scaffolding

---
+++ Controllers/TasksController.cs
@@ -0,0 +1,111 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TaskApi.Models;
+
+namespace taskapi.Controllers
+{
+    [Route("api/tasks")]
+    [ApiController]
+    public class TasksController : ControllerBase
+    {
+        private readonly TaskContext _context;
+
+        public TasksController(TaskContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TaskApi.Models.Task>>> GetTasks()
+        {
+          if (_context.Tasks == null)
+          {
+              return NotFound();
+          }
+            return await _context.Tasks.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TaskApi.Models.Task>> GetTask(long id)
+        {
+          if (_context.Tasks == null)
+          {
+              return NotFound();
+          }
+            var task = await _context.Tasks.FindAsync(id);
+
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            return task;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTask(long id, TaskApi.Models.Task task)
+        {
+            if (id != task.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(task).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TaskExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<TaskApi.Models.Task>> PostTask(TaskApi.Models.Task task)
+        {
+          if (_context.Tasks == null)
+          {
+              return Problem("Entity set 'TaskContext.Tasks'  is null.");
+          }
+            _context.Tasks.Add(task);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(TaskApi.Models.Task), new { id = task.Id }, task);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTask(long id)
+        {
+            if (_context.Tasks == null)
+            {
+                return NotFound();
+            }
+            var task = await _context.Tasks.FindAsync(id);
+            if (task == null)
+            {
+                return NotFound();
+            }
+
+            _context.Tasks.Remove(task);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool TaskExists(long id)
+        {
+            return (_context.Tasks?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
